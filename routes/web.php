55fd97d8537be1e8c5dd69d24687e4f730d77410
Gabriel Lopes <127871321+BieeLsz@users.noreply.github.com>
2024-08-16 20:28:08 -0300

    meio migrate

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FuncionarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//Funcionario
+Route::get('/funcionario', [FuncionarioController::class, 'index']);
+
